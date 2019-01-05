cf67e6bd45fbbebd8b0e4ea286436d1354ab132f
hfl <690328661@qq.com>
2019-01-05 22:44:48 +0800

    引入webpack-dev-server

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import $ from 'jquery'
 
 // const $ = require('jquery') node导入方式
 $(function () {
-    $('li:odd').css('backgroundColor','red');
+    $('li:odd').css('backgroundColor','green');
     $('li:even').css('backgroundColor',function () {
         return '#' + 'D97634';
     });
@@ -17,3 +17,18 @@ $(function () {
 //2.webpack能够处理js的兼容问题，吧高级的浏览器不识别的语法，转为低级的，浏览器能识别的语法
 //语法 webpack 要打包的文件的路径 -o 打包好的输出文件的路劲
 //webpack .\src\main.js -o .\dist\bundle.js
+
+
+
+//使用webpack-dev-server 这个工具来实现自动打包编译的功能
+//1.运行 npm i webpack-dev-server -D 吧这个工具安装到项目的本地开发依赖
+//2.安装完毕后，这个工具的用法，和webpack命令的用法，完全一样
+//webpack-dev-server这个工具，本地项目中想要用，必须安装webpack到本地
+//"webpack-cli": "^3.2.0",
+//cnpm i webpack@2.9.7 -D
+//直接托管到了电脑的内存中，所以 我们在项目根目录中，根本找不到这个打包好的bundle.ks
+//我们可以认为，webpack-dev-server 吧打包好的文件，以一种虚拟的形式，托管到了
+//我们项目中的根目录中，虽然我们看不到他，但是我们可以认为
+//和dist src node_moucle平级
+
+
